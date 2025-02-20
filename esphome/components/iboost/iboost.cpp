1837a848fcff84181a0a83f972e3446109e68251
JNSwanson <85340430+JNSwanson@users.noreply.github.com>
2025-02-20 12:36:18 +0000

    Create iboost.cpp

---
+++ esphome/components/iboost/iboost.cpp
@@ -0,0 +1,9 @@
+#include "iboost.h"
+
+namespace esphome {
+namespace iboost {
+
+iBoostBuddy *iBoost = new iBoostBuddy();  // Define the global instance
+
+}  // namespace iboost
+}  // namespace esphome

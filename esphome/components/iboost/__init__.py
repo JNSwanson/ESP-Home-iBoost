e948a9c1c2937825c52138ea836180333925b984
JNSwanson <85340430+JNSwanson@users.noreply.github.com>
2025-02-20 12:57:24 +0000

    Update __init__.py

--- esphome/components/iboost/__init__.py
+++ esphome/components/iboost/__init__.py
@@ -1,1 +1,10 @@
+import esphome.codegen as cg
+
+CODEOWNERS = ["@JNSwanson"]
+
+# Define the namespace
+iboost_ns = cg.esphome_ns.namespace("iboost")
+
+# Tell ESPHome to compile `iboost.cpp`
+cg.add_library("iboost", None)
 
